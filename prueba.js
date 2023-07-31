9defa988755e461320462a45e9a80c88802b0b7b
JoseDJarias <arias_da044@hotmail.com>
2023-07-31 08:30:15 -0600

    Primer commit

---
+++ prueba.js
@@ -0,0 +1,17 @@
+/* 
+Creamos un repositorio
+Creamos una rama llamada develop
+Creamos una funcion:
+- ImprimirNombreEdades
+- Recibir 2 argumentos
+-Primer argumento :Arreglo de nombres
+-Segundo argumento:Arreglo de edades
+-Validar que ambos arreglos sean del mismo tamaño
+['Pamela', 'Nico', 'Luna'] [31,2,2]
+-Imprimir:
+Mi nombre es Pamela y tengo 31 años
+Mi nombre es Nico y tengo 2 años
+Mi nombre es Luna y tengo 2 años
+
+Los arreglos no tienen el mismo tamaño
+*/
